532ed91cd6621789186e85e9c3a681a32f12a32c
TheSimmer101 <rachelbriskman@gmail.com>
2025-05-01 23:30:59 -0400

    added function to change background and some styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,37 @@ import './App.css'
 function App() {
   // const [count, setCount] = useState(0)
   const [index, setIndex] = useState(0);
+  //mint green, pastel yellow, pastel red, light blue, purple
+  const colorNames = ['Mint Green', 'Pastel Yellow','Pastel Red','Light Blue','Light Purple'];
+  const colorsArray = ['#67ebca','#fffeab','#ffa1a1','#a1deff', '#d5a1ff'];
+  let nextIndex = index;
+  if (index < colorNames.length - 1) {
+    nextIndex = index + 1;
+  } else {
+    nextIndex = 0;
+  }
+  
   return (
     <>
-      <div className = "container">
+      <div className = "container"
+      style={{ backgroundColor: colorsArray[index]}}>
         {
           <div className = "item">
-          <button >Click Me to Change the Background Color!</button>
+            <p>{colorNames[index] + "!"}</p>
+            <h3>The next color is: {colorNames[nextIndex]} </h3>
+          <button onClick={() => setIndex((index) => {
+            if (index < colorsArray.length - 1) {
+              console.log("index is " + index + 1);
+              return index + 1;
+            } 
+            console.log("index is 0");
+            return 0;
+          })} >
+            Click Me to Change the Background Color!
+            </button>
           </div>
         }
       </div>
-      {/* <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   )
 }
